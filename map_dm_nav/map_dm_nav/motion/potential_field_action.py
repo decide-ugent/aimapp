7d1a2fd01f1b796a70d0e6e905c243bf776c52a2
ddtinguy <daria.detinguy@ugent.be>
2025-05-26 11:17:35 +0200

    FEAT- potential field improved with vecor published + correct angle considered (if lidar not facing forward)

--- map_dm_nav/map_dm_nav/motion/potential_field_action.py
+++ map_dm_nav/map_dm_nav/motion/potential_field_action.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 import rclpy 
 from rclpy.node import Node
-from geometry_msgs.msg import Twist, Point
+from geometry_msgs.msg import Twist, PoseStamped
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 import threading
 from map_dm_nav_actions.action import PotentialField
 import numpy as np
+import tf2_ros 
 import time
 
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.action.server import ServerGoalHandle
 from rclpy.executors import MultiThreadedExecutor
 class PotentialFieldAction(Node):
-    def __init__(self, cmd_linear_max:float= 0.3, cmd_angular_max:float=0.2, 
+    def __init__(self, cmd_linear_max:float= 0.2, cmd_angular_max:float=0.2, 
                  angular_goal_tolerance:float = np.pi/10,
-                 distance_goal_tolerance:float = 0.05, obstacle_tolerance:float = 0.5):
+                 distance_goal_tolerance:float = 0.1, obstacle_tolerance:float = 0.8):
         super(PotentialFieldAction, self).__init__("PotentialFieldAction")
         
         qos_policy = rclpy.qos.QoSProfile(
@@ -32,18 +33,34 @@ class PotentialFieldAction(Node):
         
         self.odom_sub = self.create_subscription(
             Odometry,
-            '/odom',
+            '/odometry/filtered',
             self.odom_callback,
             qos_profile=qos_policy
         )
 
         self.lidar_sub = self.create_subscription(
             LaserScan,
-            'scan',
+            'scan_filtered',
             self.lidar_callback,
             qos_profile=qos_policy
         )
-  
+
+        self.apf_attraction = self.create_publisher(
+            msg_type=PoseStamped,
+            topic="APF_attraction",
+            qos_profile=qos_policy)
+        
+        self.apf_repulsion = self.create_publisher(
+            msg_type=PoseStamped,
+            topic="APF_repulsion",
+            qos_profile=qos_policy)
+        
+        self.apf_final = self.create_publisher(
+            msg_type=PoseStamped,
+            topic="APF_final",
+            qos_profile=qos_policy)
+        
+        self.tf_theta = None
         self.robot_pos_xy = None
         self.last_timer_position = None
         self.robot_theta = np.array([])
@@ -59,6 +76,9 @@ class PotentialFieldAction(Node):
         self.angular_goal_tolerance = angular_goal_tolerance
         self.distance_goal_tolerance = distance_goal_tolerance
         self.delta_distance = 0
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
         self.execution_rate =  self.create_rate(5) #sec = 1/Hz
         
@@ -85,7 +105,7 @@ class PotentialFieldAction(Node):
             robot_pose = self.robot_pos_xy + [self.robot_theta]
             if self.last_timer_position is not None \
             and np.allclose(robot_pose, self.last_timer_position, atol=0.1):
-                self.get_logger().info("Robot has not moved in the last 5 seconds. Starting abortion")
+                self.get_logger().info("Robot has not moved in the last 8 seconds. Starting abortion")
                 self.last_timer_position = None
                 self.stop_motion_timer()
                 self.abort_goal_handle()
@@ -150,7 +170,7 @@ class PotentialFieldAction(Node):
         """ get repulsion field from scan"""
 
         # Define the Field of View (e.g., 180° or 90° in radians)
-        fov_half = np.pi / 2  # 60° 
+        fov_half = np.deg2rad(30)  
 
         angle_min = lidar_msg.angle_min
         angle_max = lidar_msg.angle_max
@@ -161,33 +181,34 @@ class PotentialFieldAction(Node):
         scan = np.array(lidar_msg.ranges)
         scan_range  = len(scan)
 
-        counter = 0
-        x_r = 0
-        y_r = 0
+        x_r = 0.0000001
+        y_r = 0.0000001
+
+        if self.tf_theta is None:
+            theta_offset = 0.0
+        else:
+            theta_offset = self.tf_theta
+
+        self.V_repulsion = [x_r,y_r]
         if isinstance(self.robot_theta,np.ndarray) and len(self.robot_theta)== 0:
             return
+        self.get_logger().info(' ')
         for i in range(scan_range): 
-            angle = angle_min + step * i + self.robot_theta  # Calculate the absolute angle of the scan point
+            angle = angle_min + step * i + self.robot_theta + theta_offset # Calculate the absolute angle of the scan point
+
+            angle_difference = normalise_angle(angle - self.robot_theta) 
             # Consider only obstacles within the FOV in front of the robot
-            if -fov_half <= angle <= fov_half:
+            if abs(angle_difference) <= fov_half:
                 #If the value of the scan is > 3.5m it's above the lidar scan range
-                if(scan[i] <= range_max and scan[i] >= range_min ):
+                if(np.isfinite(scan[i]) and scan[i] <= range_max and scan[i] >= range_min):
                     Q_repulsion = 1
-                    Current_Q = (Q_repulsion) / (4 * np.pi * pow(scan[i],2))
+                    Current_Q = Q_repulsion / (4 * np.pi * pow(scan[i],2))
                     #Current_Q = Q_repulsion / pow(scan[i],2) * self.compute_force(angle, angle_max)
                     #Projection of the vectors in the x , y coordinates
-                    x_r -= Current_Q * np.cos(angle) * self.obstacle_tolerance
-                    y_r -= Current_Q * np.sin(angle) * self.obstacle_tolerance
-                else:
-                    counter += 1
-            else:
-                counter += 1
-        
-        #this consider if all the values are not in range
-        if(counter >= scan_range):
-            self.V_repulsion = [0.0001,0.000000000001]
-        else:
-            self.V_repulsion = [x_r, y_r]
+                    x_r -= Current_Q * np.cos(angle_difference) * self.obstacle_tolerance
+                    y_r -= Current_Q * np.sin(angle_difference) * self.obstacle_tolerance
+                    self.get_logger().info("reulsion at absolute: %f, relative %f, dist %f, rep x %f, y %f" % (np.rad2deg(angle), np.rad2deg(angle_difference), round(scan[i], 2), round(Current_Q * np.cos(angle_difference)* self.obstacle_tolerance,4), round(Current_Q * np.sin(angle_difference)* self.obstacle_tolerance,4)))
+        self.V_repulsion = [x_r, y_r]
 
         # angle_of_rep = np.arctan(self.V_repulsion[1]/self.V_repulsion[0])*180/np.pi
         # print(angle_of_rep)
@@ -195,7 +216,7 @@ class PotentialFieldAction(Node):
         # self.get_logger().info("the angle of repulsion is : %f" % angle_of_rep)
      
     def execute_potential_field(self, goal_handle): #, goal_pose:Point): #Goal is a point
-    
+        
         goal_pose = goal_handle.request.goal_pose
         feedback_msg = PotentialField.Feedback()
         result = PotentialField.Result()
@@ -205,6 +226,9 @@ class PotentialFieldAction(Node):
         self.start_motion_timer()
         self.execution_rate.sleep()
         goal_reached = False
+
+        if self.tf_theta is None:
+            self.get_tf_transform()
         
         while not goal_reached and rclpy.ok():
             # self.get_logger().info(str(goal_pose))
@@ -223,7 +247,8 @@ class PotentialFieldAction(Node):
             #print('reflecting')
             
             V_attraction = self.compute_attraction(np.array([goal_pose.x,goal_pose.y]))
-
+            
+            self.publish_vector(self.V_repulsion[0], self.V_repulsion[1], self.apf_repulsion)
             #angle_of_rep = np.arctan(self.V_repulsion[1]/self.V_repulsion[0])*180/np.pi
             # self.get_logger().info("the angle of repulsion is : %f" % angle_of_rep)
             # self.get_logger().info("the repulsion is : %s" % str(self.V_repulsion))
@@ -232,7 +257,7 @@ class PotentialFieldAction(Node):
             x_final = V_attraction[0] + self.V_repulsion[0]
             y_final = V_attraction[1] + self.V_repulsion[1]
             
-            
+            self.publish_vector(x_final, y_final, self.apf_final)
             angle_target = np.arctan2(y_final, x_final)
             angle_delta = angle_target - self.robot_theta
             # print('angle_delta before normalisation:', angle_delta)
@@ -302,9 +327,11 @@ class PotentialFieldAction(Node):
         #self.get_logger().info("diff goal/robot x : %f | y : %f" % (x_a,y_a))
         #self.get_logger().info("Force: %f" % F_attraction)
 
-        angle_attract = np.arctan(V_attraction[1]/V_attraction[0])*180/np.pi
+        # angle_attract = normalise_angle(np.arctan2(V_attraction[1],V_attraction[0]))
         #self.get_logger().info("angle attraction :%f°" % angle_attract)
         #self.get_logger().info("v_attraction is : x = %f ; y = %f" % (V_attraction[0],V_attraction[1]))
+
+        self.publish_vector(V_attraction[0], V_attraction[1], self.apf_attraction)
 
         return V_attraction
 
@@ -343,6 +370,24 @@ class PotentialFieldAction(Node):
             return True
         return False
     
+    def publish_vector(self,x,y, name_pub):
+        """" Visualise vectors in rviz with their intensity (topped at 7m from robot position, just for visualisation purposes)"""
+        vector = PoseStamped()
+        vector.header.frame_id = "odom"
+        vector.pose.position.x = x #max(min(x, 5.0), -5.0)
+        vector.pose.position.y = y #max(min(x, 5.0), -5.0)
+        vector.pose.position.z = 0.0
+
+        yaw = np.arctan2(y,x)
+        [x,y,z,w] = quaternion_from_euler(0,0,yaw)
+        vector.pose.orientation.x = x
+        vector.pose.orientation.y = y
+        vector.pose.orientation.z = z
+        vector.pose.orientation.w = w
+
+        name_pub.publish(vector)
+        return
+
     # def start_motion_timer(self):
     #     if self.timer is not None:
     #         self.timer.reset()
@@ -351,7 +396,7 @@ class PotentialFieldAction(Node):
     
     def create_motion_timer(self):
         if self.timer is None:
-            self.timer = self.create_timer(5.0, self.check_movement)
+            self.timer = self.create_timer(8.0, self.check_movement)
             #self.timer = threading.Timer(7.0, self.check_movement)
     
     def start_motion_timer(self):
@@ -359,11 +404,30 @@ class PotentialFieldAction(Node):
             # self.timer.start()
             self.timer.reset()
         else:
-            self.timer = self.create_timer(5.0, self.check_movement)
+            self.timer = self.create_timer(8.0, self.check_movement)
 
     def stop_motion_timer(self):
         if self.timer is not None:
             self.timer.cancel()
+
+    def get_tf_transform(self):
+        try:
+            tf = self.tf_buffer.lookup_transform(
+                'base_link',  # target frame
+                'laser',      # source frame
+                rclpy.time.Time())  # get the latest available
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            self.get_logger().warn("TF from base_link to laser not available.")
+            return 
+        
+        euler_angles = ros_quaternion_to_euler(tf.transform.rotation)
+        theta = np.round(euler_angles[2], 4)
+        #I want theta between 0-4*pi, not 0-2pi/-2pi-0
+        if theta < 0:
+            theta += 2*np.pi
+        
+        self.tf_theta = theta
+        self.get_logger().info(f'tf theta: {theta}')
            
 
 def normalise_angle(angle):
@@ -397,6 +461,25 @@ def ros_quaternion_to_euler(quaternion)-> np.ndarray:
     yaw = np.arctan2(siny_cosp, cosy_cosp)
 
     return np.array([roll, pitch, yaw])
+
+
+def quaternion_from_euler(roll, pitch, yaw):
+    """
+    Converts euler roll, pitch, yaw to quaternion
+    """
+    cy = np.cos(yaw * 0.5)
+    sy = np.sin(yaw * 0.5)
+    cp = np.cos(pitch * 0.5)
+    sp = np.sin(pitch * 0.5)
+    cr = np.cos(roll * 0.5)
+    sr = np.sin(roll * 0.5)
+
+    w = cy * cp * cr + sy * sp * sr
+    x = cy * cp * sr - sy * sp * cr
+    y = sy * cp * sr + cy * sp * cr
+    z = sy * cp * cr - cy * sp * sr
+    return [x,y,z,w]
+
 
 def min_delta(d1:list, d2:list, max_:float) -> float:
     delta = np.min([np.abs(d1 - d2), max_ - np.abs(d1 - d2)])
